cdb87bca4fc06ee07f0756e9d7f0bb75e605cfff
jaxoncreed <jaxoncreed@gmail.com>
2021-08-04 19:25:06 -0700

    Replaced Solid-Auth-Client with Solid-Client-Authn-JS

---
+++ src/authn/SolidAuthnLogic.ts
@@ -0,0 +1,18 @@
+import { NamedNode, sym } from "rdflib";
+import { AuthnLogic } from "./index";
+import { Session } from "@inrupt/solid-client-authn-browser";
+
+/**
+ * Implements AuthnLogic relying on solid-auth-client
+ */
+export class SolidAuthnLogic implements AuthnLogic {
+  private session?: Session;
+
+  constructor(solidAuthSession: Session) {
+    this.session = solidAuthSession;
+  }
+
+  currentUser(): NamedNode | null {
+    return this.session?.info.webId ? sym(this.session.info.webId) : null;
+  }
+}
--- src/authn/index.ts
+++ src/authn/index.ts
@@ -1,9 +1,5 @@
 import { NamedNode } from "rdflib";
 
-export interface Session {
-  webId: string;
-}
-
 export interface AuthnLogic {
   currentUser: () => NamedNode | null;
 }
