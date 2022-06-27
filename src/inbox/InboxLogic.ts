2fd1366124f04ea6e3a6eeeeee7a781ba428b863
ewingson <me@evering.eu>
2022-06-27 21:08:50 +0200

    setSinglePeerAccess/markAsRead

--- src/inbox/InboxLogic.ts
+++ src/inbox/InboxLogic.ts
@@ -29,7 +29,7 @@ export class InboxLogic {
     const urls = await this.util.getContainerMembers(inbox);
     return urls.filter(url => !this.util.isContainer(url));
   }
-  async createInboxFor(peerWebId: string, nick: string) {
+  async createInboxFor(ourPeerWebId: NamedNode, nick: string) {
     const myWebId: NamedNode = (await this.profile.loadMe());
     const podRoot: NamedNode = await this.profile.getPodRoot(myWebId);
     const ourInbox = `${podRoot.value}p2p-inboxes/${encodeURIComponent(nick)}/`;
@@ -38,18 +38,19 @@ export class InboxLogic {
     const aclDocUrl = await this.util.findAclDocUrl(toNode);
     await this.util.setSinglePeerAccess({
       ownerWebId: myWebId.value,
-      peerWebId,
+      peerWebId: ourPeerWebId.value,
       accessToModes: 'acl:Append',
       target: ourInbox
     });
     return ourInbox;
   }
-  async markAsRead(url: string, date: Date) {
-    const downloaded = await this.store.fetcher._fetch(url);
+  async markAsRead(url: NamedNode, date: Date) {
+    const nodeToStr = url.value;
+    const downloaded = await this.store.fetcher._fetch(nodeToStr);
     if (downloaded.status !== 200) {
       throw new Error(`Not OK! ${url}`);
     }
-    const archiveUrl = this.util.getArchiveUrl(url, date);
+    const archiveUrl = this.util.getArchiveUrl(nodeToStr, date);
     const options =  {
       method: 'PUT',
       body: await downloaded.text(),
@@ -59,7 +60,7 @@ export class InboxLogic {
     };
     const uploaded = await this.store.fetcher._fetch(archiveUrl, options);
     if (uploaded.status.toString()[0] === '2') {
-      await this.store.fetcher?._fetch(url, {
+      await this.store.fetcher?._fetch(nodeToStr, {
         method: 'DELETE'
       });
     }
