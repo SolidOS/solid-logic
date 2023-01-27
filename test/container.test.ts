c944fe03b2da92c7045307e6bb4eb72a18478476
bourgeoa <alain.bourgeois10@gmail.com>
2023-01-27 23:19:13 +0100

    test experiment

--- test/container.test.ts
+++ test/container.test.ts
@@ -28,6 +28,7 @@ describe("Container", () => {
             ].sort());
     });
     it("getContainerMembers- When container is empty - Resolves to an empty array", async () => {
+        jest.setTimeout(2000)
         containerIsEmpty();
         const result = await containerLogic.getContainerMembers('https://container.com/');
         expect(result).toEqual([]);
@@ -36,7 +37,7 @@ describe("Container", () => {
     function containerIsEmpty() {
         fetchMock.mockOnceIf(
             "https://com/",
-            " ", // FIXME: https://github.com/jefflau/jest-fetch-mock/issues/189
+            "", // FIXME: https://github.com/jefflau/jest-fetch-mock/issues/189
             {
                 headers: { "Content-Type": "text/turtle" },
             }
