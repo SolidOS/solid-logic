a2abcb4e5b64f06e07bb78ed6fb2df3729f3df3d
timea-solid <4144203+timea-solid@users.noreply.github.com>
2025-08-26 15:52:30 +0200

    updated inrupt authn lib to latest

--- src/authSession/authSession.ts
+++ src/authSession/authSession.ts
@@ -1,13 +1,7 @@
 import {
   Session,
-  getClientAuthenticationWithDependencies
 } from '@inrupt/solid-client-authn-browser'
 
-export const authSession = new Session(
-    {
-      clientAuthentication: getClientAuthenticationWithDependencies({})
-    },
-    'mySession'
-  )
+export const authSession = new Session()
 
   
