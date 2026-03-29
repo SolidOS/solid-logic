1e8752f225e8334d1246edd61404c4e2263ab683
Sharon Stratsianis <sstratsianis@gmail.com>
2026-03-29 14:23:53 +1100

    idempotent container creation

--- src/util/utilityLogic.ts
+++ src/util/utilityLogic.ts
@@ -1,106 +1,105 @@
 import { NamedNode, st, sym } from 'rdflib'
-import { CrossOriginForbiddenError, FetchError, NotEditableError, SameOriginForbiddenError, UnauthorizedError, WebOperationError } from '../logic/CustomError'
+import {
+  CrossOriginForbiddenError,
+  FetchError,
+  NotEditableError,
+  SameOriginForbiddenError,
+  UnauthorizedError,
+  WebOperationError
+} from '../logic/CustomError'
 import * as debug from '../util/debug'
 import { differentOrigin } from './utils'
 
 export function createUtilityLogic(store, aclLogic, containerLogic) {
-
-  function isNotFoundError(err: any): boolean {
-    if (err?.response?.status === 404) return true
-    const text = `${err?.message || err || ''}`
-    return text.includes('404') || text.includes('Not Found')
-  }
-
   async function recursiveDelete(containerNode: NamedNode) {
-      try {
-        if (containerLogic.isContainer(containerNode)) {
-          const aclDocUrl = await aclLogic.findAclDocUrl(containerNode)
-          await store.fetcher._fetch(aclDocUrl, { method: 'DELETE' })
-          const containerMembers = await containerLogic.getContainerMembers(containerNode)
-          await Promise.all(
-            containerMembers.map((url) => recursiveDelete(url))
-          )
-        }
-        const nodeToStringHere = containerNode.value
-        return store.fetcher._fetch(nodeToStringHere, { method: 'DELETE' })
-      } catch (e) {
-        debug.log(`Please manually remove ${containerNode.value} from your system.`, e)
+    try {
+      if (containerLogic.isContainer(containerNode)) {
+        const aclDocUrl = await aclLogic.findAclDocUrl(containerNode)
+        await store.fetcher._fetch(aclDocUrl, { method: 'DELETE' })
+        const containerMembers = await containerLogic.getContainerMembers(containerNode)
+        await Promise.all(containerMembers.map((url) => recursiveDelete(url)))
       }
+      return store.fetcher._fetch(containerNode.value, { method: 'DELETE' })
+    } catch (_e) {
+    }
   }
 
   /**
-   * Create a resource if it really does not exist
-   * Be absolutely sure something does not exist before creating a new empty file
-   * as otherwise existing could  be deleted.
-   * @param doc {NamedNode} - The resource
+   * Create a resource if it really does not exist.
+   * Be absolutely sure something does not exist before creating a new empty file,
+   * as otherwise existing content could be deleted.
    */
   async function loadOrCreateIfNotExists(doc: NamedNode) {
-    let response
     try {
-      response = await store.fetcher.load(doc)
-    } catch (err) {
-      if (err.response.status === 404) {
+      return await store.fetcher.load(doc)
+    } catch (err: any) {
+      if (err?.response?.status === 404) {
         try {
-          await store.fetcher.webOperation('PUT', doc, { data: '', contentType: 'text/turtle' })
-        } catch (err) {
-          const msg = 'createIfNotExists: PUT FAILED: ' + doc + ': ' + err
-          throw new WebOperationError(msg)
-        }
-        await store.fetcher.load(doc)
-      } else {
-        if (err.response.status === 401) {
-          throw new UnauthorizedError()
-        }
-        if (err.response.status === 403) {
-          if (differentOrigin(doc)) {
-            throw new CrossOriginForbiddenError()
+          await store.fetcher.webOperation('PUT', doc.uri, {
+            data: '',
+            contentType: 'text/turtle',
+            headers: { 'If-None-Match': '*' }
+          })
+        } catch (putErr: any) {
+          const status = putErr?.response?.status ?? putErr?.status
+          if (status !== 412) {
+            const msg = `createIfNotExists: PUT FAILED: ${doc}: ${putErr}`
+            throw new WebOperationError(msg)
           }
-          throw new SameOriginForbiddenError()
         }
-        const msg = 'createIfNotExists doc load error NOT 404:  ' + doc + ': ' + err
-        throw new FetchError(err.status, err.message + msg)
+        return await store.fetcher.load(doc)
       }
+      if (err?.response?.status === 401) {
+        throw new UnauthorizedError()
+      }
+      if (err?.response?.status === 403) {
+        if (differentOrigin(doc)) {
+          throw new CrossOriginForbiddenError()
+        }
+        throw new SameOriginForbiddenError()
+      }
+      const msg = `createIfNotExists doc load error: ${doc}: ${err}`
+      throw new FetchError(err?.status, `${err?.message || ''}${msg}`)
     }
-    return response
   }
 
   async function loadOrCreateWithContentOnCreate(doc: NamedNode, data: string): Promise<boolean> {
     try {
-      // If the document already exists, do not overwrite it; just report "not created".
       await store.fetcher.load(doc)
       return false
     } catch (err: any) {
-      if (!isNotFoundError(err)) throw err
+      const status = err?.response?.status ?? err?.status
+      if (status !== 404) {
+        throw err
+      }
     }
 
-    // At this point, the document appears to be missing. Try to create it atomically
-    // with a conditional PUT so we don't overwrite a concurrently created resource.
     try {
-      await store.fetcher.webOperation('PUT', doc, {
+      await store.fetcher.webOperation('PUT', doc.uri, {
         data,
         contentType: 'text/turtle',
         headers: { 'If-None-Match': '*' }
       })
-      await store.fetcher.load(doc)
       return true
     } catch (err: any) {
       const status = err?.response?.status ?? err?.status
       if (status === 412) {
-        // Another client created the resource between our initial 404 and this PUT.
-        // Treat it as pre-existing and do not overwrite their content.
-        await store.fetcher.load(doc)
+        // Another client created the resource between our 404 check and PUT.
         return false
       }
       throw err
     }
   }
 
-  /* Follow link from this doc to another thing, or else make a new link
-  **
-  ** @returns existing object, or creates it if non existent
-  */
-  async function followOrCreateLink(subject: NamedNode, predicate: NamedNode,
-    object: NamedNode, doc: NamedNode
+  /*
+   * Follow link from this doc to another thing, or else make a new link.
+   * Returns existing object, or creates it if non-existent.
+   */
+  async function followOrCreateLink(
+    subject: NamedNode,
+    predicate: NamedNode,
+    object: NamedNode,
+    doc: NamedNode
   ): Promise<NamedNode | null> {
     await store.fetcher.load(doc)
     const result = store.any(subject, predicate, null, doc)
@@ -111,6 +110,7 @@ export function createUtilityLogic(store, aclLogic, containerLogic) {
       debug.warn(msg)
       throw new NotEditableError(msg)
     }
+
     try {
       await store.updater.update([], [st(subject, predicate, object, doc)])
     } catch (err) {
@@ -121,7 +121,6 @@ export function createUtilityLogic(store, aclLogic, containerLogic) {
 
     try {
       await loadOrCreateIfNotExists(object)
-      // store.fetcher.webOperation('PUT', object, { data: '', contentType: 'text/turtle'})
     } catch (err) {
       debug.warn(`followOrCreateLink: Error loading or saving new linked document: ${object}: ${err}`)
       throw err
@@ -145,6 +144,7 @@ export function createUtilityLogic(store, aclLogic, containerLogic) {
       debug.warn(msg)
       throw new NotEditableError(msg)
     }
+
     try {
       await store.updater.update([], [st(subject, predicate, object, doc)])
     } catch (err) {
@@ -162,7 +162,8 @@ export function createUtilityLogic(store, aclLogic, containerLogic) {
     return object
   }
 
-  // Copied from https://github.com/solidos/web-access-control-tests/blob/v3.0.0/test/surface/delete.test.ts#L5
+  // Copied from
+  // https://github.com/solidos/web-access-control-tests/blob/v3.0.0/test/surface/delete.test.ts#L5
   async function setSinglePeerAccess(options: {
     ownerWebId: string,
     peerWebId: string,
@@ -173,12 +174,13 @@ export function createUtilityLogic(store, aclLogic, containerLogic) {
     let str = [
       '@prefix acl: <http://www.w3.org/ns/auth/acl#>.',
       '',
-      `<#alice> a acl:Authorization;\n  acl:agent <${options.ownerWebId}>;`,
+      '<#alice> a acl:Authorization;\n  acl:agent <' + options.ownerWebId + '>;',
       `  acl:accessTo <${options.target}>;`,
       `  acl:default <${options.target}>;`,
       '  acl:mode acl:Read, acl:Write, acl:Control.',
       ''
     ].join('\n')
+
     if (options.accessToModes) {
       str += [
         '<#bobAccessTo> a acl:Authorization;',
@@ -188,6 +190,7 @@ export function createUtilityLogic(store, aclLogic, containerLogic) {
         ''
       ].join('\n')
     }
+
     if (options.defaultModes) {
       str += [
         '<#bobDefault> a acl:Authorization;',
@@ -197,24 +200,22 @@ export function createUtilityLogic(store, aclLogic, containerLogic) {
         ''
       ].join('\n')
     }
+
     const aclDocUrl = await aclLogic.findAclDocUrl(sym(options.target))
     return store.fetcher._fetch(aclDocUrl, {
       method: 'PUT',
       body: str,
-      headers: [
-        ['Content-Type', 'text/turtle']
-      ]
+      headers: [['Content-Type', 'text/turtle']]
     })
   }
 
   async function createEmptyRdfDoc(doc: NamedNode, comment: string) {
     await store.fetcher.webOperation('PUT', doc.uri, {
-      data: `# ${new Date()} ${comment}
-  `,
-      contentType: 'text/turtle',
+      data: `# ${new Date()} ${comment}\n`,
+      contentType: 'text/turtle'
     })
   }
-  
+
   return {
     recursiveDelete,
     setSinglePeerAccess,
@@ -225,4 +226,3 @@ export function createUtilityLogic(store, aclLogic, containerLogic) {
     loadOrCreateWithContentOnCreate
   }
 }
-
