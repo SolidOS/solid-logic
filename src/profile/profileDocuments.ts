809edc2ad0b90efef69fbf00137fba8fc2ead5dc
Sharon Stratsianis <sstratsianis@gmail.com>
2026-03-29 20:50:48 +1100

    add prefix for terms

--- src/profile/profileDocuments.ts
+++ src/profile/profileDocuments.ts
@@ -2,6 +2,7 @@ export function preferencesFileDocument(): string {
     return [
         '@prefix dct: <http://purl.org/dc/terms/>.',
         '@prefix pim: <http://www.w3.org/ns/pim/space#>.',
+        '@prefix solid: <http://www.w3.org/ns/solid/terms#>.',
         '<>',
         '    a pim:ConfigurationFile ;',
         '    dct:title "Preferences file".'
